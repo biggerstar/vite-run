d59236585bd71146eea0487e36e4ccbfc676643c
biggerstar <1458048116@qq.com>
2024-07-10 15:40:02 +0800

    chore: 修改创建 vite-run 默认后缀 为 mts 或 mjs

--- src/bin/vite-run/createTemplate.ts
+++ src/bin/vite-run/createTemplate.ts
@@ -23,13 +23,12 @@ export function copyViteRunConfig(argMap: Record<any, any>) {
   const urls = new URL(__filename)
   const filePath = urls.pathname
   const res = glob.globSync(`${cwd()}/src/**/*.{ts,tsx}`) // 如果项目中有 ts 文件表明这是一个 ts 项目， 此时生成 ts 配置
-  let suffix = res.length > 0 ? '.ts' : '.js'
+  let suffix = res.length > 0 ? '.mts' : '.mjs'
   const configFileName = `${targetConfigName}${suffix}`
   let templateName: keyof typeof templateMapping = 'default'
   if (argMap.docs) templateName = 'docs'
   const configPath = resolve(filePath, `../../${templateMapping[templateName]}`)
   let toPath = resolve(cwd(), `./${configFileName}`)
-  if (argMap['shadow']) toPath = toPath + '.ts'   // 如果是调试情况下，为输出文件多加上一个后缀防止覆盖先工程的文件
   if (!existsSync(toPath)){ // 如果文件不存在则复制
     copyFileSync(configPath, toPath)
     console.log(colors.green('Successfully initialized viterun.config configuration file'))
